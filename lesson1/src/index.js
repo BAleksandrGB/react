f658f99d08144f814035cd0943c5aa28425991a0
BAleksandrGB <baleksandr012@yandex.ru>
2022-08-11 23:41:07 +0300

    lesson2

--- lesson1/src/index.js
+++ lesson1/src/index.js
@@ -2,16 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let links = [
+  'home',
+  'about',
+  'portfolio',
+  'contacts'
+]
+
+
+
+
+function crEl(arr) {
+  const link = React.createElement(
+    'a',
+    {
+      className: "link",
+      list: "#"
+    },
+    name
+  )
+
+  const item = React.createElement(
+    'li',
+    { className: "item" },
+    link
+  )
+}
+const menu = React.createElement(
+  'ul',
+  {
+    classname: "menu"
+  },
+  links.forEach(el=>{
+    function crEl(el)
+  })
+
+)
+
+
+ReactDOM.render(
+
+  document.getElementsById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
